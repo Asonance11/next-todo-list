088207386d82985cc52116bd617c3bd6fed6041e
Oluwadabira <adigunsamuelaso@gmail.com>
2023-12-30 19:42:35 +0100

    Create a home page sortof

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
 export default function Home() {
-	return <main className="text-3xl text-green-500">Hello World</main>;
+	return (
+		<main className="flex flex-col items-center justify-center gap-8 p-8">
+			<h2 className="text-2xl ">This is an awesome home page</h2>
+			<p>Sign in to continue</p>
+		</main>
+	);
 }
