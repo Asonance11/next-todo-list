853933e2e3b9e78293693e51d512cfbdffb35c70
Oluwadabira <adigunsamuelaso@gmail.com>
2024-01-04 13:57:18 +0100

    Removal of schema to another file

--- src/app/lib/actions.ts
+++ src/app/lib/actions.ts
@@ -2,24 +2,8 @@
 
 import bcrypt from 'bcrypt';
 import { redirect } from 'next/navigation';
-import { z } from 'zod';
 import prisma from './prisma';
-
-const UserSchema = z.object({
-	id: z.string().optional(),
-	username: z
-		.string()
-		.trim()
-		.min(3, {
-			message: 'Username must be at least 3 characters long',
-		})
-		.max(30, {
-			message: 'Username must be at most 30 characters long',
-		}),
-	hashedPassword: z.string().min(8, {
-		message: 'Password must be at least 8 characters long',
-	}),
-});
+import { UserSchema } from './zod.types';
 
 export const register = async (formData: FormData) => {
 	const validatedUser = UserSchema.safeParse({
