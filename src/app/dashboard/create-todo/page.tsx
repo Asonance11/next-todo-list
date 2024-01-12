9e16fbc4a854706beb8fce4d4e3bb1e1efe69ae3
Oluwadabira <adigunsamuelaso@gmail.com>
2024-01-12 11:55:39 +0100

    Add form for creating todos

--- src/app/dashboard/create-todo/page.tsx
+++ src/app/dashboard/create-todo/page.tsx
@@ -1,5 +1,17 @@
 const CreateTodo = () => {
-	return <div>CreateTodo</div>;
+	return (
+		<>
+			<form action="">
+				<input
+					type="text"
+					name="title"
+					id="title"
+					placeholder="What do you want to do?"
+				/>
+				<button type="submit">Submit</button>
+			</form>
+		</>
+	);
 };
 
 export default CreateTodo;
