0a252ed7bb74c9ae1e2436aaecbd8be2a255daf9
Oluwadabira <adigunsamuelaso@gmail.com>
2024-01-12 11:45:15 +0100

    Todo Pages

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,14 @@
+import Link from 'next/link';
+
 export default function Dashboard() {
-	return <div>Dashboard</div>;
+	return (
+		<section className="container mx-auto">
+			<div className="flex items-center justify-between">
+				<h1>Todos</h1>
+				<Link href="/dashboard/create-todo" className="hover:underline">
+					Create Todo
+				</Link>
+			</div>
+		</section>
+	);
 }
