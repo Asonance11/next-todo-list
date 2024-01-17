6de48cb140bad6505afbce9a794816e88e51c343
Oluwadabira <adigunsamuelaso@gmail.com>
2024-01-17 10:35:00 +0100

    Add user authentication to Navbar component

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,26 +1,46 @@
 'use client';
+
 import Link from 'next/link';
+import { useCurrentUser } from '../hooks/useCurrentUser';
+import { logout } from '../lib/actions';
 
 export default function Navbar() {
+	const user = useCurrentUser();
+
 	return (
 		<nav className="sticky bg-zinc-950 flex items-center justify-between py-4 px-8">
 			<Link href="/" className="text-2xl italic">
 				Todoist{' '}
 			</Link>
 
-			<ul className="flex items-center gap-6 ">
-				<li>
-					<Link href="/signin" className="hover:underline">
-						Sign in
-					</Link>
-				</li>
+			{user ? (
+				<ul className="flex items-center gap-6 ">
+					<li>
+						<button
+							onClick={() => {
+								logout();
+							}}
+						>
+							Logout
+						</button>
+					</li>
+					<li>{user.username}</li>
+				</ul>
+			) : (
+				<ul className="flex items-center gap-6 ">
+					<li>
+						<Link href="/signin" className="hover:underline">
+							Sign in
+						</Link>
+					</li>
 
-				<li>
-					<Link href="/register" className="hover:underline">
-						Register
-					</Link>
-				</li>
-			</ul>
+					<li>
+						<Link href="/register" className="hover:underline">
+							Register
+						</Link>
+					</li>
+				</ul>
+			)}
 		</nav>
 	);
 }
