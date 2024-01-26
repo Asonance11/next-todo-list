920b76add1ffb85ea0955044eb2095d3496f37b4
Oluwadabira <adigunsamuelaso@gmail.com>
2024-01-26 11:32:36 +0100

    Refactor TodoList component to fetch and display todos

--- src/app/components/TodoList.tsx
+++ src/app/components/TodoList.tsx
@@ -1,3 +1,17 @@
-export default function TodoList() {
-	return <div>TodoList</div>;
+import { auth } from '@/auth';
+import { getTodos } from '../lib/data';
+import TodoItem from './TodoItem';
+
+export default async function TodoList() {
+	const session = await auth();
+
+	const todos = await getTodos(session?.user?.id as string);
+
+	return (
+		<ul>
+			{todos.map((todo) => (
+				<TodoItem key={todo.id} {...todo} />
+			))}
+		</ul>
+	);
 }
