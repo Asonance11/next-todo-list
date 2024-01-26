380df6749d56beb4d2e7cbea24e8137fbefd4f86
Oluwadabira <adigunsamuelaso@gmail.com>
2024-01-26 11:41:48 +0100

    Add LogoutButton component

---
+++ src/app/components/LogoutButton.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import { logout } from '../lib/actions';
+
+const LogoutButton = () => {
+	return (
+		<button
+			onClick={() => {
+				logout();
+			}}
+		>
+			Logout
+		</button>
+	);
+};
+
+export default LogoutButton;
